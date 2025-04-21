5741e953a37f3e26df6674c72b2dc8471c69a898
Andreyrvs <andreyrvdev@gmail.com>
2025-04-21 15:10:26 -0300

    Squashed '38-Ting/' content from commit fee04c7
    git-subtree-dir: 38-Ting
    git-subtree-split: fee04c7161540f9eeca4c9d1ed7a3c5b14ab0743

---
+++ ting_file_management/file_management.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def txt_importer(path_file):
+    try:
+        if path_file[len(path_file) - 4:] != ".txt":
+            return print("Formato inválido", file=sys.stderr)
+
+        with open(path_file) as file:
+            lines = file.read()
+            return lines.split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
